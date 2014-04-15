78560b98583103050c821e801c0c48dbe23af7b1
iljaGH <ilja.kalmykov@gmx.net>
2014-04-15 17:17:05 +0200

    WS1 Tasks 1, 2
    Change folder structure. Add task 1 and 2 from the WS1. Add report
    (LaTeX and pdf) for WS1.

--- Prog/WS1T31/src/WS1T31.cpp
+++ Prog/WS1T31/src/WS1T31.cpp
@@ -3,9 +3,16 @@
 #include <gsl/gsl_rng.h>
 
 int main(){
-	//
+	/*
+	 * Use standard C function to create uniform distributed random
+	 * variables. Force conversion to double (return type of rand()
+	 * is int.
+	 */
 	printf("%lf\n", (double)rand()/RAND_MAX);
 
+	/*
+	 * Create uniform distributed random variable with GSL.
+	 */
 	gsl_rng *r = NULL;
 	r = gsl_rng_alloc(gsl_rng_mt19937);
 	printf("%lf\n", gsl_rng_uniform(r));
---
+++ Prog/WS1T32/src/WS1T32.cpp
@@ -0,0 +1,40 @@
+#include <cstdlib>
+#include <cstdio>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <fstream>
+
+int main(){
+	//#values
+	int n=1000000;
+
+	//interval bounds
+	int a=-3,b=3;
+
+	//initialize rng
+	gsl_rng* r;
+	r=gsl_rng_alloc(gsl_rng_mt19937);
+
+	//file to write to
+	std::ofstream file;
+	file.open("task2.dat");
+
+	double maxpx=gsl_ran_ugaussian_pdf(0);
+
+	for(int i=0;i<n;i++){
+	//draw x,y according to rejection sampling
+	double x=(b-a)*gsl_rng_uniform(r)+a;
+	double y=maxpx*gsl_rng_uniform(r);
+
+	//step 4 of algorithm
+	if(y<=gsl_ran_ugaussian_pdf(x))
+		file << x <<"\n";
+	else
+		i--;
+	}
+
+	file.close();
+	gsl_rng_free(r);
+
+	return 0;
+}
