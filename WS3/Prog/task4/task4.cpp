a99b35cf6a949d34cda2aa5c8860535977307d9b
bluesteaks <nullnummer1@gmx.de>
2014-06-25 18:06:04 +0200

    fixed overflow

--- WS3/Prog/task4/task4.cpp
+++ WS3/Prog/task4/task4.cpp
@@ -3,19 +3,26 @@
 #include <gsl/gsl_randist.h>
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 #include <cmath>
 #include <fstream>
 
 double discretegeometricaverage(int T, int M, int S0, int K, double sigma, double r){
 	double dt=(double)T/M;
 
+	unsigned long long b=6*M*M;
+	long long int a1=(double)M*dt*(M-1)*(4*M+1);
+	long double a2=a1/(double)b;
 
-	double T1=T-(M*(M-1)*(4*M+1))/(6.*M*M)*dt;
-	double T2=T-(M-1)/2.*dt;
+	//printf("%Lf\n",a2);
 
-	double A=exp(-r*(T-T2)-sigma*sigma*(T2-T1)/2.);
-	double d=(log((double)S0/K)+(r-0.5*sigma*sigma)*T2)/(sigma*sqrt(T1));
+	long double T1=T-a2;
+	long double T2=T-(M-1)/2.*dt;
 
+	long double A=exp(-r*(T-T2)-sigma*sigma*(T2-T1)/2.);
+	long double d=(log((double)S0/K)+(r-0.5*sigma*sigma)*T2)/(sigma*sqrt(T1));
+
+	std::cout << S0*A*gsl_cdf_gaussian_P(d+sigma*sqrt(T1),1)-K*exp(-r*T)*gsl_cdf_gaussian_P(d,1)<<"\n";
 	return S0*A*gsl_cdf_gaussian_P(d+sigma*sqrt(T1),1)-K*exp(-r*T)*gsl_cdf_gaussian_P(d,1);
 }
 
@@ -37,8 +44,10 @@ int main(){
 	double expected=continuousgeometricaverage(T,S0,K,sigma,r);
 	file << "#M error\n";
 
-	for(int i=1;i<16;i++)
+	for(int i=1;i<15;i++){
+		//printf("%f\n",discretegeometricaverage(T,pow(2,i),S0,K,sigma,r));
 		file << pow(2,i)<<" "<<std::abs(expected-discretegeometricaverage(T,pow(2,i),S0,K,sigma,r))/expected<<"\n";
+	}
 
 	file.close();
 
